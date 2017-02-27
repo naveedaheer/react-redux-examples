a5466b41999b6df18220c8405eddbf6902c568f6
naveedaheer <naveedaheer@gmail.com>
2017-02-27 20:04:55 +0500

    part 2 final commit

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {connect} from "react-redux"
+
+function mapStateToProps(state) {
+    return {
+        // counter - this will be component's property counter and can be accessed
+        // as this.props.counter
+        // state - this will be the state of redux, whatever value we return from
+        // reducer function will be in state, for now we are sending simple numaric
+        // value therefore it will be simple integer
+        counter: state,
+    };
+}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    return(
+<div><h1>React Redux App.js </h1>
+<h2>{this.props.counter}</h2></div>
     );
   }
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
--- part2/src/index.js
+++ part2/src/index.js
@@ -2,8 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import {createStore} from "redux";
+import {Provider} from "react-redux"
+
+
+function counter(value = 0, action) {
+  switch (action.type) {
+  case 'INC':
+    return value + 1
+  case 'DEC':
+    return value - 3
+  default:
+    return value
+  }
+}
+
+let store = createStore(counter)
+
+store.subscribe(() =>
+  console.log(store.getState())
+)
+
+// The only way to mutate the internal state is to dispatch an action.
+// The actions can be serialized, logged or stored and later replayed.
+store.dispatch({ type: 'INC' })
+// 1
+store.dispatch({ type: 'INC' })
+// 2
+store.dispatch({ type: 'DEC' })
+// 1
+store.dispatch({ type: 'INC' })
+store.dispatch({ type: 'DEC' })
+
 
 ReactDOM.render(
-  <App />,
+
+  //Wraping up in Provider
+  <Provider store={store}>
+  <div>
+    <App/>
+  </div>
+  </Provider>
+  ,
   document.getElementById('root')
 );
